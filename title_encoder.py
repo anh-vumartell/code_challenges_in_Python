cdcd5add91e09392cfa30a8e9b2d4e74cf3c7784
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2022-03-28 13:24:17 +0300

    task resolved

---
+++ title_encoder.py
@@ -0,0 +1,34 @@
+"""
+PROBLEM:
+- A file "books.txt" with book titles, each on a separate line
+- Encode the title: "Game of Thrones" -> "GoT"
+
+TASK:
+- Read the file
+- Encode each title
+- Output the encoded version, each on a new line
+
+WHAT TO DO:
+- Read the file, get the content into a list of titles
+- Loop through list of titles, split each title in a list of word
+- Loop through the list of word and print a result string of first letters
+"""
+
+book_file = open("texts/books.txt")
+content = book_file.readlines()
+
+for line in content:
+    # line is a list
+    str = ""
+    #loop through the list of words
+    for word in line.split():
+        str += word[0]
+        
+    print(str)
+    
+file.close()   
+        
+        
+
+
+
